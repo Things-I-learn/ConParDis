92a6e754cafda7f0378e172fbd507e2d70f4e1a1
eguar11011 <emendezc@unal.edu.co>
2026-08-08 16:28:44 -0500

    case

---
+++ Practicing_Elixir/cases.exs
@@ -0,0 +1,15 @@
+option = 2
+
+case option do
+  1 ->
+    IO.puts("You selected one")
+
+  2 ->
+    IO.puts("You selected two")
+
+  3 ->
+    IO.puts("You selected three")
+
+  _ ->
+    IO.puts("Invalid option")
+end
