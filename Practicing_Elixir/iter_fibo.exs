234f70049b11112b81f55b2f901aaeaede3e105d
eguar11011 <emendezc@unal.edu.co>
2026-08-08 17:19:02 -0500

    other solution

--- Practicing_Elixir/iter_fibo.exs
+++ Practicing_Elixir/iter_fibo.exs
@@ -17,3 +17,18 @@ end
 
 
 IterationFibo.fibo(x,a,b)|>IO.puts()
+
+
+defmodule IterationFibo do
+  def fibo(x) do
+    fibo(x, 0, 1)
+  end
+
+  def fibo(0, a, _b) do
+    a
+  end
+
+  def fibo(x, a, b) do
+    fibo(x - 1, b, a + b)
+  end
+end
