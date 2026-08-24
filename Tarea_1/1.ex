97c98616978115a19dabb5dcbfa6ce6684725e57
eguar11011 <emendezc@unal.edu.co>
2026-08-24 14:51:45 -0500

    Punto 1 resuelto

---
+++ Tarea_1/1.ex
@@ -0,0 +1,48 @@
+defmodule Ejercicio1 do
+
+  @type kind :: :equilateral | :isosceles | :scalene
+
+  def lados_numericos?(a, b, c) do
+    is_number(a) and is_number(b) and is_number(c)
+  end
+  def lados_positivos?(a,b,c) do
+    a>0 and b >0 and c> 0
+  end
+
+  def condicion_triangulo?(a,b,c) do
+    a+b >= c and a+c >= b and b+c >= a
+  end
+
+  def equilatero?(a,b,c) do
+    a == b and b == c
+  end
+
+  def isosceles?(a,b,c) do
+    a == b or b == c or a == c
+  end
+
+  def triangulo_valido?(a,b,c) do
+    lados_positivos?(a,b,c) and condicion_triangulo?(a,b,c)
+  end
+
+  @doc """
+  Return the kind of triangle with `a`, `b` and `c` as lengths.
+  """
+  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+
+  def kind(a,b,c) do
+    cond do
+      not lados_numericos?(a, b, c) -> {:error, "Invalid triangle"}
+
+      not triangulo_valido?(a,b,c) -> {:error, "Invalid triangle"}
+
+      equilatero?(a,b,c) -> {:ok, :equilateral}
+
+      isosceles?(a,b,c) -> {:ok, :isosceles}
+
+      true ->{:ok, :scalene}
+    end
+  end
+
+
+end
